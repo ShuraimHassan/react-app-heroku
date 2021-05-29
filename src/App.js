875d06ef7b0e0171e9dbf2432be6b3118e66a745
ShuraimHassan <Shuraimhassan94@gmail.com>
2021-05-30 03:41:01 +0500

    Update React App with Think Project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import Header from './Components/Header';
+// import MainSection from './Components/mainSection'
+// import Footer from './Components/footer'
+import TasbeehCounter from './Design/TasbeehCounter'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Heroku</h1>
-        <h2>This App is Push from only with main branch</h2>
-      </header>
+      <TasbeehCounter/>
     </div>
   );
 }

d90686a733d4bb3a5f8a3c823fad3fba2125e55b
ShuraimHassan <Shuraimhassan94@gmail.com>
2021-05-30 03:35:08 +0500

    second line h2 tag update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Heroku</h1>
+        <h2>This App is Push from only with main branch</h2>
       </header>
     </div>
   );

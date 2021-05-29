02faf29c98aec964b51cd1adb6352b0052b1cf35
ShuraimHassan <Shuraimhassan94@gmail.com>
2021-05-30 03:27:08 +0500

    cc

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React app to Heroku</h1>
+        <h1>Heroku</h1>
       </header>
     </div>
   );
